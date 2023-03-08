33c11f15e638fc42f3a66f7c0af55f6db4e903c3
nihs9958 <77692664+nihs9958@users.noreply.github.com>
2023-03-07 17:21:28 -0700

    Update Unit_Test.scala

--- Unit_Test.scala
+++ Unit_Test.scala
@@ -1,38 +1,28 @@
-package sal.parsing.sam.operators
-
 import org.scalatest._
-import scala.collection.mutable.HashMap
-import sal.parsing.sam.Util
-import sal.parsing.sam.SAM._
-import sal.parsing.sam.Constants._
 
 class KMediansSpec extends FlatSpec with Matchers {
+  "A KMedians operator" should "generate the correct C++ code" in {
+    val memory = Map(
+      "CurrentLStream" -> "input_stream",
+      "CurrentRStream" -> "output_stream",
+      "input_streamTupleType" -> "tuple",
+      "input_streamNumKeys" -> "1",
+      "input_streamKeyStr0" -> "key"
+    )
 
-  "K-medians operator" should "compute the median of a stream of integers" in {
+    val exp = KMediansExp("field", 5, scala.collection.mutable.HashMap(memory.toSeq: _*))
+    val opString = exp.createOpString()
 
-    // Define the input data
-    val inputData = List((1, 5), (2, 2), (3, 8), (4, 1), (5, 7))
+    val expectedOpString = """  identifier = "input_stream";
+    auto input_stream = std::make_shared<KMedians<tuple>>(5);
+    registerStream(input_stream);
+    registerStream(output_stream);
+    registerOperator(input_stream);
+    registerOperator(output_stream);
+    input_stream->registerConsumer(output_stream);
+    input_stream->initialize();
+"""
 
-    // Define the expected output
-    val expectedOutput = List((3, 5))
-
-    // Create an instance of the SAM machine
-    val sam = new SAM()
-
-    // Register the K-medians operator with the SAM machine
-    val kmediansExp = KMediansExp("field", 1, new HashMap[String, String]())
-    val opString = kmediansExp.createOpString()
-    val op = sam.compileOperator(opString)
-    sam.register(op)
-
-    // Use the input stream of the K-medians operator to feed the sample input data
-    sam.addTuple(inputData)
-
-    // Get the output from the SAM machine
-    val output = sam.getOutputStream(op).toList
-
-    // Verify that the output matches the expected output
-    output shouldEqual expectedOutput
+    opString shouldEqual expectedOpString
   }
-
 }
