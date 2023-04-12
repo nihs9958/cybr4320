8cce8df5d59e80081a1593beeb708ba3cdcf740b
nihs9958 <77692664+nihs9958@users.noreply.github.com>
2023-04-12 02:04:28 -0600

    Update and rename Unit_Test.scala to Test.scala

---
+++ Test.scala
@@ -0,0 +1,47 @@
+import org.scalatest.FlatSpec
+import sal.parsing.sam.operators.KMedians
+import sal.parsing.sam.Constants
+
+class KMediansSpec extends FlatSpec with KMedians {
+  "A k-medians operator" must "parse correctly with valid input" in {
+    val input = "kmedians(Stream1, 5)"
+    val expectedOutput = """  identifier = "Stream1";
+    auto Stream1 = std::make_shared<KMedians<TupleType>>(5);
+    addOperator(Stream1);
+    registerConsumer(Stream1, "Stream1");"""
+
+    val parsedResult = parseAll(kMediansOperator, input)
+    assert(parsedResult.successful)
+    assert(parsedResult.get.createOpString() == expectedOutput)
+  }
+
+  it must "fail to parse with invalid input" in {
+    val input = "kmedians(Stream1, invalid)"
+    val parsedResult = parseAll(kMediansOperator, input)
+    assert(parsedResult.failed)
+  }
+
+  it must "generate correct C++ code" in {
+    val input = "kmedians(Stream1, 5)"
+    val expectedOutput = """  identifier = "Stream1";
+    auto Stream1 = std::make_shared<KMedians<TupleType>>(5);
+    addOperator(Stream1);
+    registerConsumer(Stream1, "Stream1");"""
+
+    val parsedResult = parseAll(kMediansOperator, input)
+    assert(parsedResult.successful)
+    assert(parsedResult.get.createOpString() == expectedOutput)
+  }
+
+  it must "generate C++ code with the correct identifier" in {
+    val input = "kmedians(MyStream, 5)"
+    val expectedOutput = """  identifier = "MyStream";
+    auto MyStream = std::make_shared<KMedians<TupleType>>(5);
+    addOperator(MyStream);
+    registerConsumer(MyStream, "MyStream");"""
+
+    val parsedResult = parseAll(kMediansOperator, input)
+    assert(parsedResult.successful)
+    assert(parsedResult.get.createOpString() == expectedOutput)
+  }
+}
