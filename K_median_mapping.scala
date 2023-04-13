4887e7e11c9708745755f269e0e9679ddb48b271
nihs9958 <77692664+nihs9958@users.noreply.github.com>
2023-04-12 23:41:06 -0600

    Update K_median_mapping.scala

--- K_median_mapping.scala
+++ K_median_mapping.scala
@@ -7,7 +7,7 @@ import sal.parsing.sam.Util
 
 trait KMedians extends BaseParsing {
   val kMediansKeyWord: String = "kmedians"
-  
+
   def kMediansOperator: Parser[KMediansExp] =
     kMediansKeyWord ~ "(" ~ identifier ~ "," ~ int ~ ")" ^^
       { case kmed ~ lpar ~ id ~ c1 ~ k ~ rpar =>
@@ -17,19 +17,33 @@ trait KMedians extends BaseParsing {
 
 case class KMediansExp(field: String, k: Int, memory: HashMap[String, String])
     extends OperatorExp(field, memory) with Util {
-  
+
   override def createOpString(): String = {
-    val lstream = memory.get(Constants.CurrentLStream).get
-    val rstream = memory.get(Constants.CurrentRStream).get
+    val lstream = memory.getOrElse(Constants.CurrentLStream, "")
+    val rstream = memory.getOrElse(Constants.CurrentRStream, "")
 
     // Get the tuple type of the input stream
-    val tupleType = memory.get(lstream + Constants.TupleType).get
+  
+    val tupleType = memory.getOrElse(lstream + Constants.TupleType, "")
+    memory += Constants.TupleType -> tupleType
 
     // Generate SAM code for the K-medians operator
-    val opString = s"""  identifier = "$lstream";
-    auto $lstream = std::make_shared<KMedians<$tupleType>>($k);
-    ${addRegisterStatements(lstream, rstream, memory)}"""
-  
+    val opString = s"""identifier = "$field";
+    auto $field = std::make_shared<KMedians<$tupleType>>($k);
+    ${addRegisterStatements(field, rstream, memory)}"""
+
     opString
   }
+
+ override def addRegisterStatements(identifier: String, rstream: String, memory: HashMap[String, String]): String = {
+  val producer = "producer" // Replace with the appropriate producer object
+  val subscriber = "subscriber" // Replace with the appropriate subscriber object
+
+  s"""addOperator($identifier);
+  registerConsumer($identifier, "$identifier");
+  if ($subscriber != NULL) {
+    $producer->registerSubscriber($subscriber, $identifier);
+  }"""
+}
+
 }
