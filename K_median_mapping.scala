08ccf8dee272b7869219f310db0dd1fdd2bdfafc
nihs9958 <77692664+nihs9958@users.noreply.github.com>
2023-04-18 20:58:50 -0600

    Update K_median_mapping.scala

--- K_median_mapping.scala
+++ K_median_mapping.scala
@@ -18,32 +18,32 @@ trait KMedians extends BaseParsing {
 case class KMediansExp(field: String, k: Int, memory: HashMap[String, String])
     extends OperatorExp(field, memory) with Util {
 
-  override def createOpString(): String = {
-    val lstream = memory.getOrElse(Constants.CurrentLStream, "")
-    val rstream = memory.getOrElse(Constants.CurrentRStream, "")
+    override def createOpString(): String = {
+  val lstream = memory.getOrElse(Constants.CurrentLStream, "")
+  val rstream = memory.getOrElse(Constants.CurrentRStream, "")
 
-    // Get the tuple type of the input stream
-  
-    val tupleType = memory.getOrElse(lstream + Constants.TupleType, "")
-    memory += Constants.TupleType -> tupleType
+  // Get the tuple type of the input stream
+  val tupleType = memory.getOrElse(lstream + Constants.TupleType, "")
+  memory += Constants.TupleType -> tupleType
 
-    // Generate SAM code for the K-medians operator
-    val opString = s"""identifier = "$field";
-    auto $field = std::make_shared<KMedians<$tupleType>>($k);
-    ${addRegisterStatements(field, rstream, memory)}"""
+  // Generate SAM code for the K-medians operator
+  val opString = s"""identifier = "$field";
+auto $field = std::make_shared<KMedians<$tupleType>>($k);
+${addRegisterStatements(field, rstream, memory)}""".replace("$tupleType", tupleType)
 
-    opString
-  }
-
- override def addRegisterStatements(identifier: String, rstream: String, memory: HashMap[String, String]): String = {
-  val producer = "producer" // Replace with the appropriate producer object
-  val subscriber = "subscriber" // Replace with the appropriate subscriber object
-
-  s"""addOperator($identifier);
-  registerConsumer($identifier, "$identifier");
-  if ($subscriber != NULL) {
-    $producer->registerSubscriber($subscriber, $identifier);
-  }"""
+  opString
 }
+
+
+  override def addRegisterStatements(identifier: String, rstream: String, memory: HashMap[String, String]): String = {
+    val producer = "producer" // Replace with the appropriate producer object
+    val subscriber = "subscriber" // Replace with the appropriate subscriber object
+
+    s"""addOperator($identifier);
+registerConsumer($identifier, "$identifier");
+if ($subscriber != NULL) {
+  $producer->registerSubscriber($subscriber, $identifier);
+}"""
+  }
 
 }
