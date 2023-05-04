3a32df6cc0f9c75ef16a8d75bda2b1354c962a29
nihs9958 <77692664+nihs9958@users.noreply.github.com>
2023-05-04 03:30:58 -0600

    Update Test.scala

--- Test.scala
+++ Test.scala
@@ -1,62 +1,50 @@
 import org.scalatest.FlatSpec
 import sal.parsing.sam.operators.KMedians
 import sal.parsing.sam.Constants
+import sal.parsing.sam.Util
 
-class KMediansSpec extends FlatSpec with KMedians {
-  "A k-medians operator" must "parse correctly with valid input" in {
-    val input = "kmedians(Stream1, 5)"
-    val expectedOutput = """|identifier = "Stream1";
-                            |auto Stream1 = std::make_shared<KMedians<>>(5);
-                            |addOperator(Stream1);
-                            |registerConsumer(Stream1, "Stream1");
-                            |if (subscriber != NULL) {
-                            |  producer->registerSubscriber(subscriber, Stream1);
-                            |}""".stripMargin
+class KMediansSpec extends FlatSpec with KMedians with Util {
 
-    val parsedResult = parseAll(kMediansOperator, input)
-    assert(parsedResult.successful)
-    assert(parsedResult.get.createOpString() == expectedOutput)
+  "A k-medians operator" must "parse a valid input string" in {
+    val input = "kmedians(field1, 5)"
+    parseAll(kMediansOperator, input) match {
+      case Success(matched, _) =>
+        assert(matched.field == "field1")
+        assert(matched.k == 5)
+        assert(matched.memory == memory)
+      case Failure(msg, _) => fail(msg)
+      case Error(msg, _) => fail(msg)
+    }
   }
-
-  it must "fail to parse with invalid input" in {
-    val input = "kmedians(Stream1, invalid)"
+  
+  it must "fail to parse an invalid input string" in {
+    val input = "kmedians(,)"
     val parsedResult = parseAll(kMediansOperator, input)
     assert(parsedResult.isInstanceOf[Failure])
   }
 
 
-
-  
-  
   it must "generate correct C++ code" in {
-  val input = "kmedians(Stream1, 5)"
-  val expectedOutput = """|identifier = "Stream1";
-                          |auto Stream1 = std::make_shared<KMedians<>>(5);
-                          |addOperator(Stream1);
-                          |registerConsumer(Stream1, "Stream1");
-                          |if (subscriber != NULL) {
-                          |  producer->registerSubscriber(subscriber, Stream1);
-                          |}""".stripMargin
-
-  val parsedResult = parseAll(kMediansOperator, input)
-  assert(parsedResult.successful)
-  assert(parsedResult.get.createOpString() == expectedOutput)
-}
-
-  
-  
-  it must "generate C++ code with the correct identifier" in {
-    val input = "kmedians(MyStream, 5)"
-    val expectedOutput = """|identifier = "MyStream";
-                            |auto MyStream = std::make_shared<KMedians<>>(5);
-                            |addOperator(MyStream);
-                            |registerConsumer(MyStream, "MyStream");
-                            |if (subscriber != NULL) {
-                            |  producer->registerSubscriber(subscriber, MyStream);
-                            |}""".stripMargin
-
+    val input = "kmedians(field1, 3)"
+    val expectedOutput =
+      s"""|identifier = "field1";
+          |auto field1 = std::make_shared<KMedians<>>(3);
+          |addOperator(field1);
+          |registerConsumer(field1, "field1");
+          |if (subscriber != NULL) {
+          |  producer->registerSubscriber(subscriber, field1);
+          |}""".stripMargin
     val parsedResult = parseAll(kMediansOperator, input)
     assert(parsedResult.successful)
     assert(parsedResult.get.createOpString() == expectedOutput)
+  }
+
+  it must "generate C++ code with the correct identifier" in {
+    val input = "kmedians(field1, 3)"
+    val expectedIdentifier = """identifier = "field1";"""
+
+    val parsedResult = parseAll(kMediansOperator, input)
+    assert(parsedResult.successful)
+    assert(parsedResult.get.createOpString().contains(expectedIdentifier))
   }
 }
